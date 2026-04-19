de71d4a9c8ad5f056f2a8bae6cc1559e29aab4d4
brosenfelt <brosenfelt@gmail.com>
2026-04-19 19:58:49 -0400

    Upgrade to NextAuth v5

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,18 @@
-import { withAuth } from "next-auth/middleware";
+import { auth } from "@/auth";
 
-export default withAuth({
-  secret: process.env.NEXTAUTH_SECRET,
-  pages: {
-    signIn: "/login",
-  },
+export default auth((req) => {
+  const isAuth = !!req.auth;
+  const isLoginPage = req.nextUrl.pathname.startsWith("/login");
+
+  if (isLoginPage && isAuth) {
+    return Response.redirect(new URL("/overview", req.nextUrl));
+  }
+
+  if (!isLoginPage && !isAuth) {
+    return Response.redirect(new URL("/login", req.nextUrl));
+  }
 });
 
 export const config = {
-  matcher: [
-    "/((?!api/auth|_next/static|_next/image|favicon.ico|manifest.json|icon-.*\\.png|login).*)",
-  ],
+  matcher: ["/((?!api/auth|_next/static|_next/image|favicon.ico|manifest.json|icon-.*\\.png).*)" ],
 };
